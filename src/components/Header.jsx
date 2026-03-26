274205e1d07d111b7f8bba4fcedffa75f3ff258c
niiquash <qua16006@byui.edu>
2026-03-25 22:20:15 -0600

    Update imports and Header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import "./Header.css";
 
 function Header() {
   return (
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
 import "./HomePage.css";
-import "../../components/header.css";
 
 function HomePage() {
   return (
