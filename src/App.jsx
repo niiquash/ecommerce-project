27e534422f42dc6a8dc61b13f0cf6afed3de75b4
niiquash <qua16006@byui.edu>
2026-03-25 21:12:09 -0600

    Create multiple changes using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>Checkout Page</div>} />
+    </Routes>
   );
 }
 
